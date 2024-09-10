da294fe0545be650a690bb0ae4d6eb6675b04538
Lucas Johann <gabif.johann@gmail.com>
2024-09-10 15:34:22 -0300

    template de captura de dados pelo terminal

---
+++ capturaDados.js
@@ -0,0 +1,15 @@
+const readline = require('readline'); // importa o módulo readline
+
+// Cria uma interface de leitura para a entrada padrão e a saída padrão
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Pede que o usuároi digite seu nome e captura a resposta
+rl.question('Escreva seu nome: ', (nome) => {
+  console.log(`Olá, ${nome}!`); // Mostra uma saudação com o nome informado    do
+  
+  // Fecha a interface de leitura após a resposta
+  rl.close();
+})
